c640d3ee7ddb520c374f0b2943a1a43891483151
unknown <hiteshtripathi12345678@gmail.com>
2022-02-16 11:03:12 +0530

    Updates

---
+++ precache-manifest.adb0553531855df1e5504d7e0f6757b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52fd9dbb01d153faaaec9f618cf0873",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "00661c9b6ae95dc7ef1c",
+    "url": "/Tic-Tac-Toe/static/css/main.06071280.chunk.css"
+  },
+  {
+    "revision": "9f80c74d2e6b0ea807be",
+    "url": "/Tic-Tac-Toe/static/js/2.28470b7a.chunk.js"
+  },
+  {
+    "revision": "63fbe989101a4b7a3114",
+    "url": "/Tic-Tac-Toe/static/js/3.13844b13.chunk.js"
+  },
+  {
+    "revision": "00661c9b6ae95dc7ef1c",
+    "url": "/Tic-Tac-Toe/static/js/main.126061ec.chunk.js"
+  },
+  {
+    "revision": "264aa9035ab2ec0a7b19",
+    "url": "/Tic-Tac-Toe/static/js/runtime~main.ec408484.js"
+  }
+]);
